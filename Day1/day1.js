921ad73e018b87a948948460a9ef07218b146b02
Adil Basar <descrout@gmail.com>
2022-12-24 11:02:18 +0300

    javascript day1 completed

--- Day1/day1.js
+++ Day1/day1.js
@@ -1,1 +1,24 @@
-console.log("Hello world")
+const fs = require('fs');
+
+const allFileContents = fs.readFileSync('input.txt', 'utf-8');
+const inputLines = allFileContents.split(/\r?\n/);
+
+
+const latestElfCalories = [];
+let maxElfCalori = 0;
+
+for (const line of inputLines) {
+    if (line.length == 0) {
+        const totalCalori = latestElfCalories.reduce((val, sum) => val + sum);
+        if (totalCalori > maxElfCalori) {
+            maxElfCalori = totalCalori;
+        }
+        while (latestElfCalories.length) latestElfCalories.pop();
+        continue;
+    }
+
+    const calori = parseInt(line);
+    latestElfCalories.push(calori);
+}
+
+console.log(`Most carried calories: ${maxElfCalori}`);
