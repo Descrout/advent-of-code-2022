281c691d2bf61bf0cce58f84eed9e91c02216860
Adil Basar <descrout@gmail.com>
2022-12-24 13:18:52 +0300

    javascript day1 part2 completed

--- Day1/day1.js
+++ Day1/day1.js
@@ -1,14 +1,12 @@
 const inputLines = readLineByLine('input.txt');
 
 const latestElfCalories = [];
-let maxElfCalori = 0;
+const topThreeElfCalories = [0, 0, 0];
 
 for (const line of inputLines) {
     if (line.length == 0) {
         const totalCalori = latestElfCalories.reduce((val, sum) => val + sum);
-        if (totalCalori > maxElfCalori) {
-            maxElfCalori = totalCalori;
-        }
+        updateTopThree(totalCalori);
         while (latestElfCalories.length) latestElfCalories.pop();
         continue;
     }
@@ -17,7 +15,20 @@ for (const line of inputLines) {
     latestElfCalories.push(calori);
 }
 
-console.log(`Most carried calories: ${maxElfCalori}`);
+console.log(`[Part1] Most carried calories: ${topThreeElfCalories[0]}`);
+const topThreeSum = topThreeElfCalories.reduce((value, sum) => value + sum);
+console.log(`[Part2] Top three calories sum: ${topThreeSum}`);
+
+function updateTopThree(newCalories) {
+    for (let i = 0; i < topThreeElfCalories.length; i++) {
+        if (newCalories > topThreeElfCalories[i]) {
+            const oldCalori = topThreeElfCalories[i];
+            topThreeElfCalories[i] = newCalories;
+            updateTopThree(oldCalori);
+            break;
+        }
+    }
+}
 
 function readLineByLine(filePath) {
     const fs = require('fs');
