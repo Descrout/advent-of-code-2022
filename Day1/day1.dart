a32ef690d1a50490b1481c27547d379ed8ff3144
Adil Basar <descrout@gmail.com>
2022-12-24 13:12:47 +0300

    dart day1 part2 completed

--- Day1/day1.dart
+++ Day1/day1.dart
@@ -1,18 +1,15 @@
 import 'dart:io';
 
+final topThreeElfCalories = <int>[0, 0, 0];
+final latestElfCalories = <int>[];
 void main() async {
   final inputFile = File("input.txt");
   final inputLines = await inputFile.readAsLines();
 
-  final latestElfCalories = <int>[];
-  int maxElfCalori = 0;
-
   for (final line in inputLines) {
     if (line.isEmpty) {
       final totalCalori = latestElfCalories.reduce((value, sum) => value + sum);
-      if (totalCalori > maxElfCalori) {
-        maxElfCalori = totalCalori;
-      }
+      updateTopThree(totalCalori);
       latestElfCalories.clear();
       continue;
     }
@@ -21,5 +18,18 @@ void main() async {
     latestElfCalories.add(calori);
   }
 
-  print("Most carried calories: $maxElfCalori");
+  print("[Part1] Most carried calories: ${topThreeElfCalories[0]}");
+  final topThreeSum = topThreeElfCalories.reduce((value, sum) => value + sum);
+  print("[Part2] Top three calories sum: $topThreeSum");
+}
+
+void updateTopThree(int newCalories) {
+  for (int i = 0; i < topThreeElfCalories.length; i++) {
+    if (newCalories > topThreeElfCalories[i]) {
+      final oldCalori = topThreeElfCalories[i];
+      topThreeElfCalories[i] = newCalories;
+      updateTopThree(oldCalori);
+      break;
+    }
+  }
 }
