24d5b2b4688557ea9268f270b39a835f4c9551e2
Adil Basar <descrout@gmail.com>
2022-12-24 10:53:36 +0300

    golang day1 completed

--- Day1/day1.go
+++ Day1/day1.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func readLineByLine(filePath string) []string {
+	readFile, err := os.Open(filePath)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	fileLines := []string{}
+	for fileScanner.Scan() {
+		fileLines = append(fileLines, fileScanner.Text())
+	}
+
+	readFile.Close()
+
+	return fileLines
+}
+
+func sumArray(arr []int) int {
+	total := 0
+
+	for _, num := range arr {
+		total += num
+	}
+
+	return total
+}
 
 func main() {
-	fmt.Println("Hello world")
+	inputLines := readLineByLine("input.txt")
+
+	latestElfCalories := []int{}
+	maxElfCalori := 0
+
+	for _, line := range inputLines {
+		if len(line) == 0 {
+			totalCalori := sumArray(latestElfCalories)
+			if totalCalori > maxElfCalori {
+				maxElfCalori = totalCalori
+			}
+			latestElfCalories = latestElfCalories[:0]
+			continue
+		}
+
+		calori, err := strconv.Atoi(line)
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+		latestElfCalories = append(latestElfCalories, calori)
+	}
+
+	fmt.Println("Most carried calories:", maxElfCalori)
 }
