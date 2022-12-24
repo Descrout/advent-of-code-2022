830ace3fbdf26b52863dc16238bc2840d9aaa69a
Adil Basar <descrout@gmail.com>
2022-12-24 15:40:40 +0300

    dart day3 cleanup

--- Day3/day3.dart
+++ Day3/day3.dart
@@ -23,34 +23,31 @@ void main() async {
 }
 
 int findRecurringInGroup(List<String> group) {
-  final occurence = <int, int>{};
-
+  final occurences = <int, int>{};
   for (final compartment in group) {
-    putOccurences(getUniqueAscii(compartment), occurence);
-  }
-
-  int maxOccurence = 0;
-  int? maxLetter;
-  for (final kv in occurence.entries) {
-    if (kv.value > maxOccurence) {
-      maxOccurence = kv.value;
-      maxLetter = kv.key;
+    final compartmentUnique = getUniqueAscii(compartment);
+    for (final letter in compartmentUnique) {
+      final occurence = occurences[letter];
+      occurences[letter] = (occurence ?? 0) + 1;
     }
   }
+
+  final maxLetter = getHighestValuedKey(occurences);
 
   if (maxLetter == null) throw "could not find recurring letter in a group";
   return maxLetter;
 }
 
-void putOccurences(List<int> compartment, Map<int, int> occurences) {
-  for (final letter in compartment) {
-    final occurence = occurences[letter];
-    if (occurence != null) {
-      occurences[letter] = occurence + 1;
-    } else {
-      occurences[letter] = 1;
+T? getHighestValuedKey<T>(Map<T, int> map) {
+  int maxOccurence = 0;
+  T? maxKey;
+  for (final kv in map.entries) {
+    if (kv.value > maxOccurence) {
+      maxOccurence = kv.value;
+      maxKey = kv.key;
     }
   }
+  return maxKey;
 }
 
 int getSumErrors(List<String> texts) => texts.fold(
@@ -63,17 +60,13 @@ List<int> getUniqueAscii(String text) => text.codeUnits.fold(
       (previousValue, letterCode) => previousValue..add(letterCode),
     ).toList();
 
-int getPriority(int letter) {
-  if (letter > 96)
-    return letter - 96;
-  else
-    return letter - 38;
-}
+int getPriority(int letter) => letter > 96 ? letter - 96 : letter - 38;
 
 int findError(String compartment) {
   final occurence = <int>{};
   final asciiList = compartment.codeUnits;
   final half = asciiList.length / 2;
+
   for (int i = 0; i < asciiList.length; i++) {
     final codeUnit = asciiList[i];
     if (i < half) {
