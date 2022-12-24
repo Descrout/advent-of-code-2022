503030e215c85a59b75da449c8c877f53905ede9
Adil Basar <descrout@gmail.com>
2022-12-24 11:13:05 +0300

    python day1 completed

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,1 +1,18 @@
-print("Hello world")
+file = open('input.txt', 'r')
+input_lines = file.readlines()
+
+latest_elf_calories = []
+max_elf_calori = 0
+
+for line in input_lines:
+    line = line.strip()
+    if len(line) == 0:
+        total_calori = sum(latest_elf_calories)
+        if total_calori > max_elf_calori:
+            max_elf_calori = total_calori
+        latest_elf_calories.clear()
+        continue
+
+    latest_elf_calories.append(int(line))
+
+print('Most carried calories:', max_elf_calori)
