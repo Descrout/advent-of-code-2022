3a2d44225a6bf426193a90532c556331541b1871
Adil Basar <descrout@gmail.com>
2022-12-24 09:15:10 +0300

    dart day1 completed

--- Day1/day1.dart
+++ Day1/day1.dart
@@ -1,3 +1,25 @@
-void main() {
-  print("Hello world");
+import 'dart:io';
+
+void main() async {
+  final inputFile = File("input.txt");
+  final inputLines = await inputFile.readAsLines();
+
+  final latestElfCalories = <int>[];
+  int maxElfCalori = 0;
+
+  for (final line in inputLines) {
+    if (line.isEmpty) {
+      final totalCalori = latestElfCalories.reduce((value, sum) => value + sum);
+      if (totalCalori > maxElfCalori) {
+        maxElfCalori = totalCalori;
+      }
+      latestElfCalories.clear();
+      continue;
+    }
+
+    final calori = int.parse(line);
+    latestElfCalories.add(calori);
+  }
+
+  print("Most carried calories: $maxElfCalori");
 }
